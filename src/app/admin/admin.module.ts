138531ba21fa03c238db9d99256725bae9fe1a42
Sebastian Gomez <sebasgomez5892@gmail.com>
2022-09-10 15:54:44 -0500

    Agregando componente dashboard

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -7,19 +7,27 @@ import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './pages/home/home.component';
 import { ReportsComponent } from './pages/reports/reports.component';
 import { NavegacionComponent } from './components/navegacion/navegacion.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { NgChartsModule } from 'ng2-charts';
+import { TablaRankingComponent } from './components/tabla-ranking/tabla-ranking.component';
+import { TablaReportesComponent } from './components/tabla-reportes/tabla-reportes.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     ReportsComponent,
-    NavegacionComponent
+    NavegacionComponent,
+    CardsComponent,
+    TablaRankingComponent,
+    TablaReportesComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
     AdminRoutingModule,
     SharedModule,
-    MaterialModule
+    MaterialModule,
+    NgChartsModule
   ]
 })
 export class AdminModule { }
---
+++ src/app/admin/components/tabla-reportes/tabla-reportes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-reportes',
+  templateUrl: './tabla-reportes.component.html',
+  styleUrls: ['./tabla-reportes.component.scss']
+})
+export class TablaReportesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
